d6deae147c028d8850aeb056ddc75caa782a10c2
caitsith2 <d_good@caitsith2.com>
2020-04-12 14:32:16 -0700

    Add Dungeon Counter Options

--- Rom.py
+++ Rom.py
@@ -1126,9 +1126,11 @@ def patch_rom(world, rom, player, team, enemized):
     rom.write_byte(0x18003B, 0x01 if world.mapshuffle[player] else 0x00)  # maps showing crystals on overworld
 
     # compasses showing dungeon count
-    if world.clock_mode[player]:
+    if world.clock_mode[player] or not world.dungeon_counters[player]:
         rom.write_byte(0x18003C, 0x00)  # Currently must be off if timer is on, because they use same HUD location
-    elif world.compassshuffle[player]:
+    elif world.dungeon_counters[player] == True:
+        rom.write_byte(0x18003C, 0x02)  # always on
+    elif world.compassshuffle[player] or world.dungeon_counters[player] == 'pickup':
         rom.write_byte(0x18003C, 0x01)  # show on pickup
     else:
         rom.write_byte(0x18003C, 0x00)
